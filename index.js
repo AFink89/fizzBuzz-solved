4450719990321a512da378c81b591cde4f7866c6
AMaupin <alexandrafink8989@gmail.com>
2025-08-08 10:08:19 -0700

    scrapped original, use for statement to add number to allow increase, create 3x else if statements, 1x else statement no more infinite loop

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var number = 1;
-
-while (number < 100) {
-  if (number % 3 === 0 && number % 5 == 0) {
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  }
-  else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
   }
 }
